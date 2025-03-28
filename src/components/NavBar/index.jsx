74cf8c632dcffd0d40a686b056a9984b552047f2
israel guedes <israelguedes008@gmail.com>
2025-03-28 02:31:33 -0300

    ajietando login parte 2

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import "./NavBar.css";
+import "./navbar.css";
 import Avatar from "./assets/avatar.svg";
 
 const NavBar = () => {
